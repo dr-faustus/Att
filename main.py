271fc6c6cccb78375871f79631604879fa6dab35
dr-faustus <s.movahedi@icloud.com>
2018-08-22 15:57:03 +0430

    --

--- main.py
+++ main.py
@@ -58,7 +58,6 @@ learning_rate_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
 learning_rate = 0.001
 batch_size = 128
 
-hidden_size = 128
 input_size = 300
 topic_hidden_size = 20
 drop_out_prob = 0.6
@@ -72,16 +71,22 @@ early_stopping_patience = 10
 
 dataset_name = 'sem-2016'
 num_of_topics = 11
+hidden_size = 128
 # num_of_topics_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics_list, hidden_size,
 # topic_hidden_size, drop_out_prob)
-hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics, hidden_size_list,
-topic_hidden_size, drop_out_prob)
+# hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics, hidden_size_list,
+# topic_hidden_size, drop_out_prob)
+topic_hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type,
+                             num_of_topics, hidden_size, topic_hidden_size_list, drop_out_prob)
 dataset_name = 'sem-2014'
 num_of_topics = 6
+hidden_size = 128
 # num_of_topics_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics_list, hidden_size,
 # topic_hidden_size, drop_out_prob)
-hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics, hidden_size_list,
-topic_hidden_size, drop_out_prob)
+# hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type, num_of_topics, hidden_size_list,
+# topic_hidden_size, drop_out_prob)
+topic_hidden_size_validation(300, dataset_name, learning_rate, batch_size, model_type,
+                             num_of_topics, hidden_size, topic_hidden_size_list, drop_out_prob)
 # test(learning_rate, batch_size, dataset_name, model_type, early_stopping_mode, early_stopping_min_delta, early_stopping_patience)
 # sentence_weight_examine(6)
 # examine_context_vectors(15, './topic-attention')
